c66ec107ae180ec4fee944ba73d029e4c52b15fd
Heimdall Sham <heim.sham@icloud.com>
2021-09-21 22:37:29 -0400

    Created custom workout page.

--- client/src/page/CustomizeSelection.js
+++ client/src/page/CustomizeSelection.js
@@ -1,24 +1,31 @@
 import React, {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 import {API_URL} from '../utils';
+import './CustomizeSelection.scss';
 
 function CustomizeSelection() {
-    const [muscle, setPath] = useState(null);
-    const {muscleId} = useParams(); 
+    const [muscles, setGroup] = useState([]);
 
     useEffect(() => {
-        axios.get(`${API_URL}/customize/${muscleId}`).then((response) => {
-            setPath(response.data.muscle);
+        axios.get(`${API_URL}muscle`).then((response) => {
+            setGroup(response.data.results);
         })
-    }, [muscleId]);
+    }, []);
 
     return (
-        <Link to="/customize/muscleId" className="muscle__card">
-            <img className="workout__muscle-img"></img>
-            <h2 className="workout__muscle">Muscle Title</h2>
-        </Link>
+        <main>
+            <h1 className="customize__title">Custom Workout</h1>
+            {muscles.map((group) => (
+            <Link to={"customize/" + group.id} key={group.id} className="customize__card">
+                <div className="customize__image-container">
+                    <img src="https://wger.de/static/images/muscles/muscular_system_front.svg" className="customize__full-body"/>
+                    <img src={`https://wger.de${group.image_url_main}`} className="customize__muscle-image"/>
+                </div>
+                <div className="customize__muscle">{group.name}</div>
+            </Link>
+            ))}
+        </main>
     )
 }
 
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+
+const PORT = 8000;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log("Server is good to go")
+})
