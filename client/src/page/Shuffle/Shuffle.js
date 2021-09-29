87fd6d8e926a5728234f7e576707b77c98b645c3
Heimdall Sham <heim.sham@icloud.com>
2021-09-28 22:58:05 -0400

    Modal added for front end for shuffle.

--- client/src/page/Shuffle/Shuffle.js
+++ client/src/page/Shuffle/Shuffle.js
@@ -3,10 +3,12 @@ import MuscleSelection from '../../components/MuscleSelection/MuscleSelection';
 import {API_URL} from '../../utils';
 import { EXP_URL } from '../../utils';
 import axios from 'axios';
+import Modal from 'react-modal';
 
 function Shuffle() {
     const [muscles, setMuscles] = useState([]);
     const [exercise, setExercise] = useState([]);
+    const [showSubmitModal, setShowSubmitModal] = useState(false)
     // an array of muscle id's that a user has selected
     const [selectedMuscleIds, setSelectedMuscleIds] = useState([]);
 
@@ -32,16 +34,28 @@ function Shuffle() {
         setSelectedMuscleIds(newSelectedMuscleIds);
     }
 
-    const handlePost = () => {
+    const handlePost = (event) => {
+        event.preventDefault();
+        const data = {
+            muscleIds: selectedMuscleIds, 
+            title: event.target.title.value,}
             axios({
                 method: "POST",
                 url: (`${EXP_URL}shuffle`),
-                data: selectedMuscleIds,
+                data: data,
             }).then (response => {
                 console.log(response)
             })
         }
-        
+
+    function openSubmitModal() {
+        setShowSubmitModal(true)
+    }
+
+    function closeSubmitModal() {
+        setShowSubmitModal(false)
+    }
+
     return (
         <div>
             <h1 className="shuffle__title">Shuffle</h1>
@@ -54,7 +68,20 @@ function Shuffle() {
                 <MuscleSelection group={muscleGroup} />
             </div>
             ))}
-            <button type="submit" className="shuffle__button" onClick={() => handlePost()}>Shuffle</button>
+            <Modal
+            isOpen={showSubmitModal}
+            onRequestClose={closeSubmitModal}
+            className="exercise__submit-modal"
+            >
+                <form onSubmit={handlePost}>
+                    <div className="exercise__input-container">
+                        <div className="exercise__title-header">Routine Title:</div>
+                        <input type="text" className="exercise__title" name="title"></input>
+                    </div>
+                        <button className="exercise__submit-button" type="submit">Submit</button>
+                </form>
+            </Modal>
+            <button className="shuffle__button" onClick={openSubmitModal}>Shuffle</button>
         </div>
     )
 }
--- server/routers/shuffleRouter.js
+++ server/routers/shuffleRouter.js
@@ -12,9 +12,15 @@ const readExercise = () => {
     return parseExerciseData;
 }
 
+
 router.post('/', (req, res) => {
     console.log("req test", req.body)
     return res.status(200).json({status: "ok"})
+
+    //We have the muscle id's 
+    // for each muscle id we have, we want to get a list of all eligible exercises
+    // from the list of eligible exercises, we will randomly pick 3 
+    // for each exercise, we will randomly generate a random number of sets and reps
 })
 
 module.exports = router; 
